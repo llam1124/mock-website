eef49ebcebf6827d3a029bae8bee5225c3d0435a
Lilian <llam1124@hotmail.com>
2020-08-31 14:26:06 -0400

    added google captcha component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -10,7 +10,7 @@
             <div id="contact_form">
                 <p style="padding-top: 10px;"> Want to learn more about Nissint? Do you have a comment or suggestion?</p>
                 <p>We would love to hear from you!</p>
-                <v-form ref="form"  align="right">
+                <v-form ref="form">
                     <v-col>
                         <v-text-field 
                             v-model="name" 
@@ -27,7 +27,17 @@
                             label="message" 
                             outlined ></v-textarea>
                     </v-col>
-                    <v-btn dark>Submit</v-btn>
+                    <!--CAPTCHA HERE-->
+                    <div class="captcha">
+                        <vue-recaptcha
+                            ref="recaptcha"
+                            @expired="onCaptchaExpired"
+                            sitekey="6Ldq28UZAAAAAKcYtg2X9NNnDrdkSm9NVpLqIwKB" 
+                            :loadRecaptchaScript="true"
+                        >
+                        </vue-recaptcha>
+                        <v-btn class="mt-4" dark>Submit</v-btn>
+                    </div>
                 </v-form>
             </div> 
         </div> 
@@ -35,13 +45,61 @@
 </template>
 
 <script>
+import VueRecaptcha from 'vue-recaptcha';
 export default {
     name: "Contact",
+    components: {
+        'vue-recaptcha': VueRecaptcha
+    },
     data: () => ({
         name: '',
         email: '',
         message: '',
-    })
+    }),
+    methods: {
+        submit: function () {
+            // this.status = "submitting";
+            this.$refs.recaptcha.execute();
+        },
+        /*
+        onCaptchaVerified: function (recaptchaToken) {
+            const self = this;
+            self.status = "submitting";
+            self.$refs.recaptcha.reset();
+            axios.post("https://vue-recaptcha-demo.herokuapp.com/signup", {
+                email: self.email,
+                password: self.password,
+                recaptchaToken: recaptchaToken
+            }).then((response) => {
+                self.sucessfulServerResponse = response.data.message;
+            }).catch((err) => {
+                self.serverError = getErrorMessage(err);
+
+
+                //helper to get a displayable message to the user
+                function getErrorMessage(err) {
+                let responseBody;
+                responseBody = err.response;
+                if (!responseBody) {
+                    responseBody = err;
+                }
+                else {
+                    responseBody = err.response.data || responseBody;
+                }
+                return responseBody.message || JSON.stringify(responseBody);
+                }
+
+            }).then(() => {
+                self.status = "";
+            });
+
+
+        },
+         */
+        onCaptchaExpired: function () {
+            this.$refs.recaptcha.reset();
+        }
+    }
 }
 </script>
 
@@ -54,6 +112,10 @@ export default {
         margin: 20px 0px 20px 0px; /* top right bottom left*/
         font-size: 20px;
         color: #888;
+    }
+    .captcha {
+        margin-left: 15px;
+        margin-bottom: 10px;
     }
     #contact_content {
         display: flex;
@@ -86,6 +148,7 @@ export default {
         }
 
         #contact_info {
+            width: 100%;
             flex-direction: column;
             margin-top: 20px;   
         }
